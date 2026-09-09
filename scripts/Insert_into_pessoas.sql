138f1d4a1e8beb4b805824e7d9499cfaeecb8806
Judson Gabriel <165574074+JudGB@users.noreply.github.com>
2026-09-08 23:43:15 -0400

    Delete scripts/Insert_into_pessoas.sql

--- scripts/Insert_into_pessoas.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO pessoas (cpf, nome)
-VALUES
-    ('12345678901', 'João da Silva'),
-    ('23456789012', 'Maria Santos'),
-    ('34567890123', 'Pedro Oliveira'),
-    ('45678901234', 'Ana Souza'),
-    ('56789012345', 'Carlos Pereira');
-
