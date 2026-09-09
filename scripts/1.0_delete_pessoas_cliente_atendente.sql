f79026931bd7baf50a295cdf605792f936b64364
Judson Gabriel <165574074+JudGB@users.noreply.github.com>
2026-09-08 23:56:38 -0400

    Versão final dos delets
    Juntando e adicionando os últimos deletes

--- scripts/1.0_delete_pessoas_cliente_atendente.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM atendentes;
-DELETE FROM clientes;
-DELETE FROM pessoas;
---
+++ scripts/2.0_DELETE_all_tabelas.sql
@@ -0,0 +1,63 @@
+-- CONTRATOS --
+
+DELETE FROM contratos
+WHERE id_contrato = 1;
+
+DELETE FROM contratos
+WHERE id_contrato = 2;
+
+DELETE FROM contratos
+WHERE id_contrato = 3;
+
+DELETE FROM contratos
+WHERE id_contrato = 4;
+
+-- VEICULOS --
+
+DELETE FROM veiculos
+WHERE id_veiculo = 1;
+
+DELETE FROM veiculos
+WHERE id_veiculo = 2;
+
+DELETE FROM veiculos
+WHERE id_veiculo = 3;
+
+DELETE FROM veiculos
+WHERE id_veiculo = 4;
+
+-- ATENDENTES --
+
+DELETE FROM atendentes
+WHERE id_atendente = 1;
+
+DELETE FROM atendentes
+WHERE id_atendente = 2;
+
+DELETE FROM atendentes
+WHERE id_atendente = 3;
+
+-- CLIENTES --
+
+DELETE FROM clientes
+WHERE id_cliente = 1;
+
+DELETE FROM clientes
+WHERE id_cliente = 2;
+
+-- PESSOAS --
+
+DELETE FROM pessoas
+WHERE id_pessoa = 1;
+
+DELETE FROM pessoas
+WHERE id_pessoa = 2;
+
+DELETE FROM pessoas
+WHERE id_pessoa = 3;
+
+DELETE FROM pessoas
+WHERE id_pessoa = 4;
+
+DELETE FROM pessoas
+WHERE id_pessoa = 5;
